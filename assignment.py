35169a81252801602a91bb3e87fce1b2eb8a7571
Vinod ravi <114280598+vinodravi84@users.noreply.github.com>
2022-10-16 11:25:07 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,22 @@
+list=[1,2,3,4,5,6,7,8,9,0]
+k=len(list)
+print(k)
+
+string1='Vinod'
+print(string1[:5])
+for i in range(len(string1)):
+    print (string1[i])
+
+
+
+list.append(11)
+print(list)
+
+
+
+list2=['v','i','n','o','d']
+list.extend(list2)
+print(list)
+
+
+print(list.pop(5))
---
+++ operator.py
@@ -0,0 +1,31 @@
+a=5
+b=10
+print(a+b)#addition
+print(a-b)#subtraction
+print(a*b)#multiplication
+print(a/b)#division
+print(a%b)#modulus
+print(a//b)#quotient
+print(a**b)#exponentiation
+print(a==b)#condition to check if both are equal
+print(a!=b)#condition to check if both are not equal
+print(a>b)#greater
+print(a<b)#less than
+print(a<=b)#less than or equal to
+print(a>=b)#greater than or equal to
+a+=b#a=a+b
+print(a)
+a-=b#a=a-b
+print(a)
+a*=b#a=a*b
+print(a)
+a/=b#a=a/b
+print(a)
+
+#--------------------------------------------------------------------------------------
+
+c=[10,100]
+d=[10,100]
+e=c
+print(c is d)#true if both variables have same value and same memory location
+print(c is not d)#true if both variables do not have same value and same memory location
